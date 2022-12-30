a07081eb163ee6e6371a38125c148da98693e8e9
EJ <ejoh@bu.edu>
2022-12-30 13:28:50 -0500

    create parameterized report example

---
+++ 02_render_param_report.R
@@ -0,0 +1,49 @@
+
+# Render R Markdown using R Script - Parameterized ------------------------
+# 2022-12-19
+
+library(readr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(janitor)
+library(lubridate)
+library(rmarkdown)
+library(here)
+
+d0 <- read_csv(here("data", "pho_data_tool_case_trends_2022-12-19.csv"))
+d0 <- clean_names(d0)
+
+d <- d0 %>% 
+  mutate(new_date = as.Date(date, format = "%B %d, %Y")) %>% 
+  mutate(across(where(is.character), ~ifelse(. == "-", NA, .))) %>% 
+  mutate(yr = year(new_date)) %>% 
+  mutate(z = as.numeric(format(new_date, "%m%d"))) %>% 
+  group_by(yr) %>% 
+  mutate(day = row_number() + min(z) - 99) %>% 
+  ungroup() %>% 
+  mutate(day = yday(new_date)) %>% 
+  mutate(out_date = format(new_date, "%b %d")) %>% 
+  select(new_date, public_health_unit, yr, day, out_date, matches("^cases|hosp|death")) 
+
+
+# Parameterized Report ----------------------------------------------------
+# pass parameters into R Markdown file, in example is public health unit `phu`
+set.seed(32)
+phus <- c("Ottawa Public Health", "Toronto Public Health", "Peel Public Health")
+phus
+for (i in phus) {
+  render(
+    input = here("rmd", "02_param_report.rmd"),
+    output_format = "html_document",
+    output_file = paste0("COVID_hosp_ON_", i, "_", Sys.Date()),
+    output_dir = here("rmd_output"),
+    clean = TRUE,
+    quiet = FALSE,
+    encoding = "UTF-8",
+    params = list(
+      phu = i
+    )
+  )
+}
+
